30bfeb621c33ad0206b2394cfaec2dc28288426a
emcassi <alex.wayne.dev@gmail.com>
2022-11-16 09:25:33 -0700

    Initial commit. UI setup, retrieves hobby

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
+import Button from "./Button";
+import HobbyView from "./HobbyView";
 
 function App() {
+  const [hobby, setHobby] = useState();
+
+  const handleButtonClicked = (event) => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "9fa785df16msh0d11cacf77192a8p107815jsnaf73c34e9334",
+        "X-RapidAPI-Host": "hobbies-by-api-ninjas.p.rapidapi.com",
+      },
+    };
+
+    fetch(
+      "https://hobbies-by-api-ninjas.p.rapidapi.com/v1/hobbies?category=general",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setHobby(response.hobby);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <HobbyView hobby={hobby} />
+      <Button image="arrows-rotate.svg" style={{backgroundColor: "#C89933"}} onClick={handleButtonClicked}>Get new hobby</Button>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import styles from "./Button.module.css"
+
+function Button(props) {
+  return (
+    <button className={styles.button} onClick={props.onClick}>
+        <img src={props.image} alt="" />
+        <p>{props.children}</p>
+    </button>
+  )
+}
+export default Button
